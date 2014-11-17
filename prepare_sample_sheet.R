21f640e23b5531dfe218039af1b71fed800ca971
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2014-11-17 14:15:29 -0500

    prepare_sample_sheet.R: The script no longer crash when there is no paired end files
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- prepare_sample_sheet.R
+++ prepare_sample_sheet.R
@@ -4,7 +4,7 @@ input <- argv[1]
 output <- argv[2]
 
 ### Prepare metadata object
-metadata <- read.csv(input)
+metadata <- read.csv(input, stringsAsFactors = FALSE)
 single <- which(metadata[["run_type"]] != "Paired-ended")
 paired <- which(metadata[["run_type"]] == "Paired-ended"
               & metadata[["paired_end"]] == 1)
@@ -45,23 +45,25 @@ sample_sheet[["Run Type"]][1:length(single)] <- "SINGLE_END"
 sample_sheet["Filename Prefix"] <- sample_sheet[["Name"]]
 sample_sheet["FASTQ1"] <- paste(sample_sheet[["Name"]], "fastq.gz", sep = ".")
 
-## Add FASTQ files and paired Name
-# In sample_sheet data.frame, the first rows are the single ended files.
-# So if we start a length(single)+1, we can be sure were are in the
-# paired files section of sample_sheet.
-for (i in (length(single)+1):nrow(sample_sheet)) {
-  # Set the correct Name and Filename Prefix
-  tokens <- unlist(strsplit(sample_sheet[["Name"]][i], "_"))
-  pair_1 <- tokens[5]
-  pair_2 <- metadata[metadata[["paired_with"]] == pair_1,][["file_accession"]]
-  pair_2 <- as.character(pair_2)
-  new_name <- paste(c(tokens[1:5], pair_2, tokens[6:length(tokens)]), collapse = "_")
-  sample_sheet[["Name"]][i] <- new_name
-  sample_sheet[["Filename Prefix"]][i] <- new_name
-  # Add FASTQ1 and FASTQ2
-  sample_sheet[["FASTQ1"]] <- paste0(paste(tokens, collapse="_"), ".fastq.gz")
-  fastq_2 <- paste(c(tokens[1:4], pair_2, tokens[6:length(tokens)]), collapse = "_")
-  sample_sheet[["FASTQ2"]] <- paste0(fastq_2, ".fastq.gz")
+if (length(paired) > 0) {
+  ## Add FASTQ files and paired Name
+  # In sample_sheet data.frame, the first rows are the single ended files.
+  # So if we start a length(single)+1, we can be sure were are in the
+  # paired files section of sample_sheet.
+  for (i in (length(single)+1):nrow(sample_sheet)) {
+    # Set the correct Name and Filename Prefix
+    tokens <- unlist(strsplit(sample_sheet[["Name"]][i], "_"))
+    pair_1 <- tokens[5]
+    pair_2 <- metadata[metadata[["paired_with"]] == pair_1,][["file_accession"]]
+    pair_2 <- as.character(pair_2)
+    new_name <- paste(c(tokens[1:5], pair_2, tokens[6:length(tokens)]), collapse = "_")
+    sample_sheet[["Name"]][i] <- new_name
+    sample_sheet[["Filename Prefix"]][i] <- new_name
+    # Add FASTQ1 and FASTQ2
+    sample_sheet[["FASTQ1"]] <- paste0(paste(tokens, collapse="_"), ".fastq.gz")
+    fastq_2 <- paste(c(tokens[1:4], pair_2, tokens[6:length(tokens)]), collapse = "_")
+    sample_sheet[["FASTQ2"]] <- paste0(fastq_2, ".fastq.gz")
+  }
 }
 
 #### Save results
